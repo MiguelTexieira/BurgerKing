869aad3e206cb96b544b20db2e2a6e3891eb7b2a
Miguel Teixeira <miguel@techin.co.uk>
2018-01-05 08:08:32 +0000

    Did some reducer refactoring

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -40,6 +40,7 @@ class BurgerBuilder extends Component {
 	}
 
 	purchaseContinueHandler = () => {
+		this.props.onInitPurchase();
 		this.props.history.push('/checkout');
 	}
 
@@ -88,9 +89,9 @@ class BurgerBuilder extends Component {
 
 const mapStateToProps = state => {
 	return {
-		ings: state.ingredients,
-		price:	state.totalPrice,
-		error: state.error
+		ings: state.burgerBuilder.ingredients,
+		price:	state.burgerBuilder.totalPrice,
+		error: state.burgerBuilder.error
 	};
 }
 
@@ -98,7 +99,8 @@ const mapDispatchToProps = dispatch => {
 	return {
 		onIndgredientAdded: (ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)),
 		onIndgredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName)),
-		onInitIngredients: () => dispatch(burgerBuilderActions.initIngredients())
+		onInitIngredients: () => dispatch(burgerBuilderActions.initIngredients()),
+		onInitPurchase: () => dispatch(burgerBuilderActions.purchaseInit())
 	}
 }
 
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -87,13 +87,12 @@ class ContactData extends Component{
 						{value: 'cheapest', displayValue: 'Cheapest'}
 					]
 				},
-				value: '',
+				value: 'fastest',
 				validation: {},
 				valid: true
 			}
 		},
-		formIsValid: false,
-		loading: false
+		formIsValid: false
 	}
 
 	orderHandler = (e) => {
@@ -179,7 +178,7 @@ class ContactData extends Component{
 					))}
 					<Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button> 
 				</form>);
-		if (this.state.loading) {
+		if (this.props.loading) {
 			form = <Spinner />;
 		}
 		return (
@@ -193,8 +192,9 @@ class ContactData extends Component{
 
 const mapStateToProps = state => {
 	return {
-		ings: state.ingredients,
-		price: state.totalPrice
+		ings: state.burgerBuilder.ingredients,
+		price: state.burgerBuilder.totalPrice,
+		loading: state.order.loading
 	}
 };
 
